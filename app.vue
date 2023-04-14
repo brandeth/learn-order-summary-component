1d229ff874adaf325190aea000bfa2167e1034e3
Rey Garcia <jreygarcia24@gmail.com>
2023-04-15 00:49:20 +0800

    added target blank

--- app.vue
+++ app.vue
@@ -13,7 +13,7 @@ useHead({
 
     <footer class="attribution">
       <p>Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a></p>
-      <p>Coded by <a href="https://github.com/brandeth">Rey Garcia</a></p>
+      <p>Coded by <a href="https://github.com/brandeth" target="_blank">Rey Garcia</a></p>
     </footer>
   </div>
 </template>
